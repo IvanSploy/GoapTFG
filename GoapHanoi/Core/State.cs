487baed44b296418c4982b71c5c5b054c20bb6a2
Iván Rodríguez García <ivansploy672@gmail.com>
2022-11-16 02:06:17 +0100

    State Base
    Se crea el funcionamiento base de State que almacenará las reglas del mundo cuya base establecerá GOAP.

--- GoapHanoi/Core/State.cs
+++ GoapHanoi/Core/State.cs
@@ -4,7 +4,67 @@ namespace GoapHanoi.Core
 {
     public class State<TA, TB>
     {
-        private HashSet<Property<TA, TB>> values;
+        private Dictionary<TA, TB> _values;
+
+        public State()
+        {
+            _values = new Dictionary<TA, TB>();
+        }
         
+        public State(Dictionary<TA, TB> values)
+        {
+            _values = values;
+        }
+        
+        //GOAP Utilities, A* addons.
+        public bool IsConflictive(State<TA, TB> state, out int mismatches)
+        {
+            mismatches = 0;
+            foreach (var pair in state._values)
+            {
+                if (Has(pair.Key))
+                {
+                    if (HasValue(pair.Key, pair.Value))
+                    {
+                        mismatches++;
+                    }
+                }
+            }
+            
+            return mismatches == 0;
+        }
+
+        //Dictionary
+        public void Set(TA key, TB value)
+        {
+            _values[key] = value;
+        }
+        
+        public void Remove(TA key)
+        {
+            _values.Remove(key);
+        }
+
+        private bool Has(TA key)
+        {
+            return _values.ContainsKey(key);
+        }
+        
+        private bool HasValue(TA key, TB value)
+        {
+            return _values[key].Equals(value);
+        }
+        
+        //Operators
+        public static State<TA, TB> operator +(State<TA, TB> a, State<TA, TB> b)
+        {
+            State<TA, TB> state = new State<TA, TB>(a._values);
+            foreach (var pair in b._values)
+            {
+                state._values[pair.Key] = pair.Value;
+            }
+            
+            return state;
+        }
     }
 }
