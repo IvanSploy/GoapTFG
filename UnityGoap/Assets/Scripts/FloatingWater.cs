032d969585f2f78da9bed063463cb33109227aed
IvanSploy <ivansploy672@gmail.com>
2023-03-23 02:43:44 +0100

    ADD - New scenary
    +Añadido escenario basado en una ciudad costera, alpha.

---
+++ UnityGoap/Assets/Scripts/FloatingWater.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FloatingWater : MonoBehaviour
+{
+    public float density = 1;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        var t = other.transform;
+        var pos = t.position;
+        pos.y += Time.fixedDeltaTime * density * 10;
+        t.position = pos;
+    }
+}
